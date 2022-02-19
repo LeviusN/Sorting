55d695c6676d339c94e14066b6e72ab5fa148a09
LeviusN <91337423+LeviusN@users.noreply.github.com>
2022-02-19 17:56:34 +0100

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -88,3 +88,63 @@ def heapify(list, n,i):
 arr = [ 5, 4,8,6,20,50,12]
 heapSort(arr)
 print (arr)
+
+
+#------------------------------DFS-------------------------------------------------
+
+class Node:
+    def __init__(self,value,left=None,right=None):
+        self.value=value
+        self.left=left
+        self.right=right
+    def __str__(self):
+        return "Node(" +str(self.value) +")"
+
+def walk(tree):
+    if tree is not None:
+        print(tree)
+        walk(tree.left)
+        walk(tree.right)
+
+mytree=Node('A', Node('B', Node('D'), Node('E')), Node('C', Node('F'), Node('G')))
+walk(mytree)
+
+#-----------------------------BFS---------------------------------------------------------
+class Node:
+    def __init__(self,value,left=None,right=None):
+        self.value=value
+        self.left=left
+        self.right=right
+    def __str__(self):
+        return "Node(" +str(self.value) +")"
+
+
+def bfs(node,queue):
+    queue.append(node)
+    while len(queue) > 0:
+        node = queue.pop(0)
+        if node is not None:
+            print(node)
+            queue.append(node.left)
+            queue.append(node.right)
+            
+mytree=Node('A', Node('B', Node('D'), Node('E')), Node('C', Node('F'), Node('G')))
+bfs(mytree, [])
+
+#-------------------------------BFS2--------------------------------------------------
+
+def bfs(graph, root):
+    visited = set()
+    queue = [root]
+    
+    while queue:
+        vertex=queue.pop()
+        visited.add(vertex)
+        for i in graph[vertex]:
+            if i not in visited:
+                queue.append(i)
+    print(visited)
+    
+if __name__ == "__main__":
+    graph={0:[1,2,3], 1:[0,2,4], 2:[0,1,5], 3:[0], 4:[1], 5:[2]}
+    bfs(graph,0)
